7139ad2dc0a847c97b5fd579722782a5213df38e
Sigmund05 <Sigmund05@proton.me>
2026-09-03 14:10:41 +0000

    pdx246: Import device manifest file from stock Based on 70.2.A.4.168
    Change-Id: Ic79a81d864d2fb5e384ae35632e3354249361905

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,11 @@ TARGET_BOOTLOADER_BOARD_NAME := parrot
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest_parrot.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 

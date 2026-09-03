09ad8f7cc0606f26e85c53aabd76ac66e20aff2a
Sigmund05 <Sigmund05@proton.me>
2026-09-03 13:42:32 +0000

    pdx246: Create QCOM common mountpoints
    Change-Id: Id6f8cd00326d91e7a2b966d3084731ea8c75be6d

--- device.mk
+++ device.mk
@@ -3,6 +3,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_vm-system_mountpoint
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \

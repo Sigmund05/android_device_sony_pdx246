4e6620a6384f6cb7d24ca7d15dd88fc8f2896cb5
Sigmund05 <Sigmund05@proton.me>
2026-09-03 13:25:46 +0000

    pdx246: Define architecture, bootloader and platform
    Change-Id: I6d90a2e9739109794ca04b20c53eba7f5312971c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,25 @@
 
 DEVICE_PATH := device/sony/pdx246
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo300
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := parrot
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := parrot
+
 # Inherit the proprietary files
 include vendor/sony/pdx246/BoardConfigVendor.mk
